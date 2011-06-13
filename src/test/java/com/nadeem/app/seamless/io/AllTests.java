010c8be6bcf7e0eb2006ad72c93bb4991c1fa0ac
Mohammad Nadeem <nadeembtech@gmail.com>
2011-06-13 23:47:09 +0530

    All Test file

---
+++ src/test/java/com/nadeem/app/seamless/io/AllTests.java
@@ -0,0 +1,21 @@
+package com.nadeem.app.seamless.io;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.nadeem.app.seamless.io.handler.config.FileWritingConfigTest;
+import com.nadeem.app.seamless.io.session.DefaultFileSessionTest;
+import com.nadeem.app.seamless.io.session.ftp.FtpSessionFactoryTest;
+import com.nadeem.app.seamless.io.session.ftp.FtpSessionTest;
+
+@RunWith(Suite.class)
+@SuiteClasses(
+	{DefaultFileSessionTest.class,
+	DefaultFileSessionTest.class,
+	FtpSessionFactoryTest.class,
+	FtpSessionTest.class,
+	FileWritingConfigTest.class})
+public class AllTests {	
+
+}
